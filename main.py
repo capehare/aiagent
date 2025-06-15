7b95ed33ef88d6013bfebd659480929f6bcfffb8
capehare <cape@haremail.slmail.me>
2025-06-15 14:54:03 +0200

    Modified main.py to allow entry of prompt from the command line. Modified .gitignore.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+try:
+    query = sys.argv[1:]
+    joined_query = " ".join(query)
+    assert joined_query != ""
+except (IndexError, AssertionError):
+    sys.stderr.write("Please enter a prompt.\n")
+    sys.exit(1)
+
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001", 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    contents=joined_query
+    )
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 print(response.text)
